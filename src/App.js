18aa62063d5e9035c8e7188287a985aad9a477e0
kanghea <kanghea26@gmail.com>
2022-07-14 12:11:35 +0900

    df

--- src/App.js
+++ src/App.js
@@ -1,14 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import './App.css';
 
-function Food(props) {
-  return <h1>hello world { props.fav}</h1>
+function HeaderBenner() {
+  const [show, setShow] = useState(true);
+  return <div>
+    {
+      show &&
+      <div className="w-full bg-orange-400 h-16">
+        <div className="align-middle text-center pt-5 text-base">
+          <a href="/read1">
+            <span className="text-white mt-3">
+              대구 부동산 연구소 상담문의 바로가기
+            </span>
+          </a>
+          <button type="button" class="pl-10 text-orange-100 inline-flex items-center justify-center hover:text-orange-500 focus:ring-inset" onClick={() => {
+            setShow(false);
+          }}>
+            <span class="sr-only">Close menu</span>
+            <svg class="h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+          </button>
+        </div>
+      </div>
+    }
+  </div>
 }
+function Body() {
+  return <div>
+    <div className="w-full bg-white h-28">
+      <div>
+        <a href="/">
+          <svg className="-auto "></svg>
+        </a>
+      </div>
+
+      <div>  
+      </div>
+    </div>
+  </div>
+}
+
+
+
 
 function App() {
   return (
     <div>
-      <h1>hello world</h1>
-      <food fav="kimchi" />
+      <HeaderBenner />
+      <Body />
     </div>
   );
 }
