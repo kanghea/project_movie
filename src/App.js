b09b95b87073f8272419234ecd8cb625078bfef4
kanghea <kanghea26@gmail.com>
2022-07-15 20:01:02 +0900

    d

--- src/App.js
+++ src/App.js
@@ -6,19 +6,19 @@ function HeaderBenner() {
   return <div>
     {
       show &&
-      <div className="w-full bg-orange-400 h-16">
+      <div className="w-full bg-orange-400 h-16 antialiased">
         <div className="flex columns-5 items-center h-full">
           <div className="flex-none grow col-span-1"></div>
           <a href="/read1">
             <span className="text-white mt-3 col-span-1 pl-10">
-                <b>대구 부동산 연구소 상담문의 바로가기 ➜</b>
+                <strong>대구 부동산 연구소 상담문의 바로가기 ➜</strong>
             </span>
           </a>
           <a href="/read1">  
             <div className="basis-5"></div>
           </a>
-          <a href="/read1" className="w-auto h-full pl-10">
-            <img src="img/lol.png" className="w-auto h-full pl-10"></img>
+          <a href="/read1" className="w-auto h-full lg:pl-10">
+            <img src="img/lol.png" className="w-auto h-full lg:pl-10"></img>
           </a>
           <div className="flex-none grow col-span-1"></div>
           <button type="button" className="flex-none col-span-1 text-orange-100 hover:text-orange-500 pl-auto pr-52" onClick={() => {
@@ -34,37 +34,11 @@ function HeaderBenner() {
     </div>}
   </div>
 }
-function HeaderB() {
-  const [event, setEvent] = useState(true);
-  return <>
-    {
-      event &&
-      <div className="w-full h-24 opacity-90 fixed blur-sm">
-        <div className="flex flex-row grow-0 pl-52 pr-52 pt- bg-black align-middle">
-          <div className="flex-none basis-48 w-40">
-            <a href="/read">
-              <img alt="logo" src="img/logo.png" />
-            </a>
-          </div>
-          <div className="basis-1/2 grow"></div>
-          <div className="flex-none basis-96 w-72 pt-8">
-            <div className="text-xs flex flex-row">
-              <a className="basis-48 color-white bg-orange-400 text-white border-solid rounded-3xl h-10 align-middle text-center pt-2 hover:bg-orange-500 text-sm border-2 border-orange-300" href="/"><b> 대.부.연 앱 다운하기 ➜</b></a>
-              <div className="basis-4"></div>
-              <a className="basis-48 bg-slate-100 border-solid rounded-3xl h-10 align-middle text-center pt-2 hover:bg-slate-200 text-sm border-2 border-gray-300" href="/"><b>대.부.연 웹사이트 ➜</b></a>
-            </div>
-          </div>
-        </div>
-      </div>
-      
-    }</>
-
-}
-
 function Header() {
   return <>
-    <div className="w-full h-28">
-      <div className="h-full flex grow-0 bg-white justify-between pl-52 pr-52">
+  {
+    <div className="w-full h-28 antialiased">
+      <div className="w-full h-full flex grow-0 bg-white justify-between content-center lg:px-52 sticky top-0">
         <div className="flex-none basis-48 w-40">
           <a href="/">
             <img alt="logo" src="img/logo.png" />
@@ -72,16 +46,17 @@ function Header() {
         </div>
         <div className="flex-none basis-96 w-72 pt-8">
           <div className="text-xs flex flex-row">
-            <a className="basis-48 color-white bg-orange-400 text-white border-solid rounded-3xl h-10 align-middle text-center pt-2 hover:bg-orange-500 text-sm border-2 border-orange-300" href="/"><b> 대.부.연 앱 다운하기 ➜</b></a>
+            <a className="basis-48 color-white bg-orange-400 text-white border-solid rounded-3xl h-10 align-middle text-center pt-2 hover:bg-orange-500 text-sm border-2 border-orange-300" href="/"><strong> 대.부.연 앱 다운하기 ➜</strong></a>
             <div className="basis-4"></div>
-            <a className="basis-48 bg-slate-100 border-solid rounded-3xl h-10 align-middle text-center pt-2 hover:bg-slate-200 text-sm border-2 border-gray-300" href="/"><b>대.부.연 웹사이트 ➜</b></a>
+            <a className="basis-48 bg-white border-solid rounded-3xl h-10 align-middle text-center pt-2 hover:bg-slate-100 text-sm border-2 border-gray-300" href="/"><strong>대.부.연 웹사이트 ➜</strong></a>
           </div>
         </div>
       </div>
     </div>
-
+  }
   </>
 }
+
 
 
 
@@ -91,7 +66,6 @@ function App() {
     <div>
       <HeaderBenner />
       <Header />
-      <HeaderB />
     </div>
   );
 }
