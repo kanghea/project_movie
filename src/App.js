b9f9f8de5c09a24ececa0d0aae7782ce9c3d903c
kanghea <kanghea26@gmail.com>
2022-07-15 13:40:18 +0900

    d
    d

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function HeaderBenner() {
     {
       show &&
       <div className="w-full bg-orange-400 h-16">
-        <div className="flex columns-3 items-center h-full">
+        <div className="flex columns-4 items-center h-full">
+          <div className="basis-52 grow"></div>
           <div className="flex-none grow col-span-1"></div>
           <a href="/read1">
             <span className="text-white mt-3">
@@ -15,7 +16,7 @@ function HeaderBenner() {
             </span>
           </a>
           <div className="flex-none grow col-span-1"></div>
-          <button type="button" className="flex-none col-span-1 pl-24 text-orange-100 hover:text-orange-500 pr-auto" onClick={() => {
+          <button type="button" className="flex-none col-span-1 text-orange-100 hover:text-orange-500 pl-auto" onClick={() => {
             setShow(false);
           }}>
           <span class="sr-only">Close menu</span>
@@ -23,6 +24,7 @@ function HeaderBenner() {
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
           </svg>
         </button>
+        <div className="basis-52 grow"></div>
       </div>
     </div>}
   </div>
@@ -50,11 +52,11 @@ function Header() {
           </a>
         </div>
         <div className="basis-1/2 grow"></div>
-        <div className="flex-none basis-80 w-72 pt-8">
+        <div className="flex-none basis-96 w-72 pt-8">
           <div className="text-xs flex flex-row">
-            <a className="basis-40 color-white bg-orange-400 text-white border-solid rounded-3xl h-10 align-middle text-center pt-2 hover:bg-orange-500 text-sm border-2 border-orange-300" href="/"><b>대.부.연 앱 다운하기 ➜</b></a>
+            <a className="basis-48 color-white bg-orange-400 text-white border-solid rounded-3xl h-10 align-middle text-center pt-2 hover:bg-orange-500 text-sm border-2 border-orange-300" href="/"><b> 대.부.연 앱 다운하기 ➜</b></a>
             <div className="basis-4"></div>
-            <a className="basis-40 bg-slate-100 border-solid rounded-3xl h-10 align-middle text-center pt-2 hover:bg-slate-200 text-sm border-2 border-gray-300" href="/"><b>대.부.연 웹사이트 ➜</b></a>
+            <a className="basis-48 bg-slate-100 border-solid rounded-3xl h-10 align-middle text-center pt-2 hover:bg-slate-200 text-sm border-2 border-gray-300" href="/"><b>대.부.연 웹사이트 ➜</b></a>
           </div>
         </div>
       </div>
