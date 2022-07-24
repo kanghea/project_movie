22afd26b5fa31c9e9473edbe09bf0d1904f83cb0
kanghea <kanghea26@gmail.com>
2022-07-24 14:43:53 +0900

    Update Act.js
    this is push X

--- src/components/Act.js
+++ src/components/Act.js
@@ -5,9 +5,9 @@ function Act() {
     const [act2, setAct2] = useState(false);
     const [act3, setAct3] = useState(false);
     return <>
-      <div className="w-full h-auto flex flex-wrap mb-36 gap-10 mt-5 xl:px-26 justify-center">
-        <a className='w-[525px]'>
-          <div className="mt-5 w-[525px] bg-gray-100 hover:bg-blue-400 hover:mt-0 hover:mb-5 duration-100 h-[632px] rounded-[40px] p-10 hover:text-white hover:border-white border-stone-400 ">
+      <div className="w-full h-auto flex flex-wrap mb-36 gap-10 mt-5 xl:px-26 justify-center transition-all">
+        <a className='w-[525px] box-border '>
+          <div className="mt-5 w-[525px] bg-gray-100 hover:bg-blue-400 hover:mt-0 hover:mb-5 duration-200 h-[632px] rounded-[40px] p-10 hover:text-white hover:border-white border-stone-400 ">
             <div className="text-[40px] leading-[60px] font-semibold">매물등록</div>
             <div className="text-lg font-normal">소장이 하나하나 검토해서 신뢰성있는 매물 多<br></br> 인공지능 가격분석으로 합리적인 가격까지</div>
             <div className="pt-5">
@@ -17,7 +17,7 @@ function Act() {
           </div>
         </a>
         <a href="/" className='w-[525px] h-[532px]'>
-          <div className="mt-5 w-[525px] bg-gray-100 hover:bg-green-400 hover:mt-0 hover:mb-5 duration-100 h-[532px] rounded-[40px] p-10 hover:text-white hover:border-white border-stone-400">
+          <div className="mt-5 w-[525px] bg-gray-100 hover:bg-green-400 hover:mt-0 hover:mb-5 duration-200 h-[532px] rounded-[40px] p-10 hover:text-white hover:border-white border-stone-400">
             <div className="text-[40px] leading-[60px] font-semibold">지도기반 매물 표시 </div>
             <div className="text-lg font-normal">신뢰성 높은 매물과 합리적인 가격과<br></br> 가독성 높은 지도 UI</div>
             <div className="pt-5">
@@ -27,7 +27,7 @@ function Act() {
           </div>
         </a>
         <a href="/" className='w-[525px]'>
-          <div className="mt-5 bg-gray-100 hover:bg-yellow-400 hover:mt-0 hover:mb-5 duration-100 h-[632px] rounded-[40px] p-10 hover:text-white hover:border-white border-stone-400 ">
+          <div className="mt-5 bg-gray-100 hover:bg-yellow-400 hover:mt-0 hover:mb-5 duration-200 h-[632px] rounded-[40px] p-10 hover:text-white hover:border-white border-stone-400 ">
             <div className="text-[40px] leading-[60px] font-semibold">매물광고</div>
             <div className="text-lg font-normal">소장이 하나하나 검토해서 신뢰성있는 매물 多<br></br> 인공지능 가격분석으로 합리적인 가격까지</div>
             <div className="pt-5">
@@ -36,7 +36,7 @@ function Act() {
           </div>
         </a>
         <a href="/" className='w-[525px]'>
-          <div className="mt-5 w-[525px] bg-gray-100 hover:bg-red-400 hover:mt-0 hover:mb-5 duration-100 h-[632px] rounded-[40px] p-10 hover:text-white hover:border-white border-stone-400 ">
+          <div className="mt-5 w-[525px] bg-gray-100 hover:bg-red-400 hover:mt-0 hover:mb-5 duration-200 h-[632px] rounded-[40px] p-10 hover:text-white hover:border-white border-stone-400 ">
             <div className="text-[40px] leading-[60px] font-semibold">매물광고</div>
             <div className="text-lg font-normal">소장이 하나하나 검토해서 신뢰성있는 매물 多<br></br> 인공지능 가격분석으로 합리적인 가격까지</div>
             <div className="pt-5">
@@ -47,14 +47,20 @@ function Act() {
       </div>
       {act1&&
         <div>
-          <div className='z-10 w-full h-full bg-black/70 fixed top-0 left-0' onClick={() => { setAct1(false); }}></div>
-          <div className='z-20 fixed w-full h-full justify-center items-center align-middle'>
-            <div className='flex fixed w-[720px] h-[600px] p-8 bg-white rounded-[40px]' onClick={() => { setAct1(true); }}>
+          <div className='z-10 w-full h-full bg-black/70 fixed top-0 left-0 flex justify-center items-center'>
+            <div className='flex w-[720px] h-[600px] p-8 bg-white rounded-[40px]'>
               <div className='absolute w-[640px] border-b-gray-400'>
                 <div className='text-2xl mb-6 font-semibold'>매물등록</div>
-                <ul className='flex gap-12 text-xl text-gray-400 w-full border-b-2 border-black font-[550] pb-2'>
-                  <li className='hover:text-black hover:bg-gray-100 hover:rounded-3xl hover:w-auto hover:h-8'>간편한 매물 등록</li>
-                  <li className='hover:text-black'>철저한 매물 관리</li>
+                <ul className='flex gap-12 text-xl text-gray-400 border-b-[1px] border-gray-400 font-[550] pb-2'>
+                  <li className= 'hover:text-black focus:text-black cursor-pointer'>간편한 매물 등록</li>
+                  <li className='hover:text-black cursor-pointer'>철저한 매물 관리</li >
+                  <button type="button" className="flex-none col-span-1 text-gray-400 hover:text-gray-800 ml-auto" onClick={() => {
+                    setAct1(false);
+                  }}>
+                    <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                  </button>
                 </ul>
               </div>
             </div>
