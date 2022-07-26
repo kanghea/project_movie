8c727850e63eb8ca6f5f73a0db89813b2ebf0808
kanghea <kanghea26@gmail.com>
2022-07-27 08:58:34 +0900

    fed

--- src/map.js
+++ src/map.js
@@ -1,11 +1,12 @@
 import React, {useState} from "react";
 import MapAPI from "./mapcomponents/mapAPI";
+import Mapmain from "./mapcomponents/mapmain";
 function Map(){
     return(
         <div>
+            <Mapmain/>
             <MapAPI/>
         </div>
     )
 }
-
 export default Map;
---
+++ src/mapcomponents/mapmain.js
@@ -0,0 +1,15 @@
+function Mapmain(){
+    return(
+        <div className="p-0 m-0">
+            <div className="w-full h-15 bg-black/30 float-left">
+                <ul className="px-3">
+                    <li>1</li>
+                    <li>2</li>
+                    <li>3</li>
+                    <li>4</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+export default Mapmain;
